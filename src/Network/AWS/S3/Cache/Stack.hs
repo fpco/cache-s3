39bf4ba8bfd16eb6738d24787afb5b0089e13a3d
Alexey Kuleshevich <alexey@fpcomplete.com>
2017-12-11 01:32:03 +0300

    debug git branch names

--- src/Network/AWS/S3/Cache/Stack.hs
+++ src/Network/AWS/S3/Cache/Stack.hs
@@ -45,9 +45,7 @@ getStackGlobalPaths mStackRoot = do
 getStackResolver :: StackProject -> IO T.Text
 getStackResolver (StackProject { stackResolver = Just resolver }) = return resolver
 getStackResolver (StackProject { stackYaml = mStackYaml }) = do
-  stackYaml <- case mStackYaml of
-    Just stackYaml -> return stackYaml
-    Nothing -> fromMaybe "stack.yaml" <$> lookupEnv "STACK_YAML"
+  stackYaml <- getStackYaml mStackYaml
   eObj <- decodeFileEither stackYaml
   case eObj of
     Left exc -> throwIO exc
@@ -59,13 +57,11 @@ getStackResolver (StackProject { stackYaml = mStackYaml }) = do
 
 
 
-getStackYaml :: [String] -> Maybe FilePath -> IO FilePath
-getStackYaml args mStackYaml =
+getStackYaml :: Maybe FilePath -> IO FilePath
+getStackYaml mStackYaml =
   case mStackYaml of
     Just stackYaml -> return stackYaml
-    Nothing        -> do
-
-      getStackPath args "--config-location"
+    Nothing -> fromMaybe "stack.yaml" <$> lookupEnv "STACK_YAML"
 
 
 getStackWorkPaths :: Maybe FilePath -- ^ Stack root. It is needed in order to prevent stack from
@@ -75,47 +71,27 @@ getStackWorkPaths :: Maybe FilePath -- ^ Stack root. It is needed in order to pr
                   -> Maybe FilePath -- ^ Relative path for --work-dir
                   -> IO [FilePath]
 getStackWorkPaths mStackRoot mStackYaml mWorkDir = do
-  let args = getStackRootArg mStackRoot
-      fromStr (String str) = Just $ T.unpack str
-      fromStr _            = Nothing
-  stackYaml <- getStackYaml args mStackYaml
-  projectRoot <- getStackPath (args ++ ["--stack-yaml", stackYaml]) "--project-root"
+  let fromStr (String str) = Just $ T.unpack str
+      fromStr _ = Nothing
+  stackYaml <- getStackYaml mStackYaml
+  projectRoot <-
+    getStackPath (getStackRootArg mStackRoot ++ ["--stack-yaml", stackYaml]) "--project-root"
   workDir <-
     case mWorkDir of
       Just workDir -> return workDir
-      Nothing      -> fromMaybe ".stack-work" <$> lookupEnv "STACK_WORK"
+      Nothing -> fromMaybe ".stack-work" <$> lookupEnv "STACK_WORK"
   eObj <- decodeFileEither stackYaml
   pathPkgs <-
     case eObj of
       Left exc -> throwIO exc
-      Right (Object (HM.lookup "packages" -> mPackages)) | isJust mPackages ->
-        case mPackages of
-          Just (Array v) -> return $ V.toList (V.mapMaybe fromStr v)
-          _ -> error $ "Expected 'packages' to be a list in the config: " ++ stackYaml
+      Right (Object (HM.lookup "packages" -> mPackages))
+        | isJust mPackages ->
+          case mPackages of
+            Just (Array v) -> return $ V.toList (V.mapMaybe fromStr v)
+            _ -> error $ "Expected 'packages' to be a list in the config: " ++ stackYaml
       _ -> error $ "Couldn't find 'packages' in the config: " ++ stackYaml
   return $ map (\pkg -> projectRoot </> pkg </> workDir) pathPkgs
 
-
-upgradeStack :: Maybe FilePath -- ^ Stack root
-             -> IO ()
-upgradeStack mStackRoot =
-  callProcess "stack" (getStackRootArg mStackRoot ++ ["upgrade"])
-
-
--- | Try to install stack. Returns `True` if installation went successful, `False` if stack was
--- already installed (no upgrade is attempted here, use `upgradeStack` for that), and throws an
--- error if there was some problem.
-installStack :: Maybe FilePath -- ^ Stack root
-             -> IO Bool
-installStack mStackRoot = do
-  -- check if stack is already installed.
-  (eCode, _, _) <- readProcessWithExitCode "stack" (getStackRootArg mStackRoot ++ ["--version"]) ""
-  case eCode of
-    ExitSuccess -> return False
-    _           -> do
-      -- TODO: implement downloading of stack binary for the specific platform and placing it into a
-      -- system dependant local/bin
-      return True
 
 -- | Will do its best to find the git repo and get the current branch name, unless GIT_BRANCH env
 -- var is set, in which case its value is returned.
